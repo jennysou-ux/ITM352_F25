3c42b0e0c8c8da188a4335579cb273c08b458bf5
jennysou-ux <jennysou@hawaii.edu>
2025-09-25 11:36:29 -1000

    Lab 6 changes

---
+++ Lab 6/Ex1.py
@@ -0,0 +1,6 @@
+# Initialize a tuple with different emotions 
+# Name: Jenny Soukhaseum
+# Date: 09/24/2025
+
+emotions = ("happy", "sad", "angry", "excited", "bored")
+print(emotions[0] == "happy" and len(emotions) > 3)
---
+++ Lab 6/Ex1b.py
@@ -0,0 +1,10 @@
+# Initialize a tuple with different emotions 
+# Name: Jenny Soukhaseum
+# Date: 09/24/2025
+
+emotions = ("happy", "sad", "angry", "excited", "bored")
+if (len(emotions) > 3 and emotions[0] == "happy"):
+    print(True)
+else: 
+    print("Len is not > 3 or someone isn't happy")
+    
