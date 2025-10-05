a282590543e3c88047c40f9e3c7b6b9814e709de
jennysou-ux <jennysou@hawaii.edu>
2025-10-04 16:36:05 -1000

    Forgot to do Ex5 Part b

---
+++ Lab 7/Ex5b.py
@@ -0,0 +1,12 @@
+celebrities_tuple = ("Taylor Swift", "Lionel Messi", "Max Verstappen", "Keanu Reeves", "Angelina Jolie")
+ages_tuple = (34, 36, 26, 60, 48)
+
+celebrities_list = list(celebrities_tuple)
+ages_list = list(ages_tuple)
+
+celebrities_dictionary = {
+    "Celebrities": celebrities_list,
+    "Ages": ages_list
+}
+
+print(celebrities_dictionary)
